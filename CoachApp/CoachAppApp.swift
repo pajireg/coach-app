01cbb17e57749ae7b181f76791b02c64e10e90a2
Sumin Choi <pajireg@gmail.com>
2026-04-28 07:43:06 +0900

    Initial Commit

---
+++ CoachApp/CoachAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachAppApp.swift
+//  CoachApp
+//
+//  Created by Sumin Choi on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct CoachAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
